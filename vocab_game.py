8e1fde0a90aa8352d810bc3fa5ccb006e1f59600
403-11-teethat <54426@yupparaj.ac.th>
2026-08-31 13:11:58 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -46,14 +46,14 @@ def show_result_dialog(ans1, ans2, ans3, ans4):
     else:
         st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
 
-     # ตรวจข้อ 3
+    # ตรวจข้อ 3
     if u_ans3 == "banana":
         st.success("✅ ข้อ 3: ถูกต้อง")
         score += 1
     else:
         st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
 
-     # ตรวจข้อ 2
+    # ตรวจข้อ 4
     if u_ans4 == "glasses":
         st.success("✅ ข้อ 4: ถูกต้อง")
         score += 1
